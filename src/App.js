2b18980b1824c6df558d377025d7e3001e65ffff
Simon Tollstern <simontollstern@gmail.com>
2019-12-19 14:33:38 +0100

    Add very basic socket.io implementation

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import openSocket from 'socket.io-client';
+const socket = openSocket('http://localhost:8000');
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props); 
+    this.state = {
+      value: '',
+      messages: []
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentDidMount(){
+    socket.on("message", message => {
+      this.setState({ messages: [...this.state.messages, message] })
+    })
+  }
+
+  handleChange(e){
+    this.setState({ value: e.target.value });
+  }
+
+  handleSubmit(e){
+    e.preventDefault();
+    socket.emit("message", this.state.value)
+    this.setState({ value: '' })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-intro">
+          <p>socket.io test</p>
+          <form onSubmit={this.handleSubmit}>
+            <input type="text" value={this.state.value} onChange={this.handleChange}></input>
+            <input type="submit"></input>
+          </form>
+          <ul>
+            {this.state.messages.map((message, i) => (<li key={i}>{message}</li>))}
+          </ul>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
