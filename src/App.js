8a06a7c5fae59097f468a206f9626ea292907adc
Simon Tollstern <simontollstern@gmail.com>
2019-12-20 09:44:24 +0100

    Add user id to messages and fix eslint errors

--- src/App.js
+++ src/App.js
@@ -1,47 +1,54 @@
 import React, { Component } from 'react';
 import './App.css';
 import openSocket from 'socket.io-client';
+
 const socket = openSocket('http://localhost:8000');
 
 class App extends Component {
   constructor(props) {
-    super(props); 
+    super(props);
     this.state = {
       value: '',
       messages: []
-    }
+    };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  componentDidMount(){
-    socket.on("message", message => {
-      this.setState({ messages: [...this.state.messages, message] })
-    })
+  componentDidMount() {
+    // fix this eslint error later, idk how though :)
+
+    socket.on('message', message => {
+      this.setState({ messages: [...this.state.messages, message] });
+    });
   }
 
-  handleChange(e){
+  handleChange(e) {
     this.setState({ value: e.target.value });
   }
 
-  handleSubmit(e){
+  handleSubmit(e) {
+    const { value } = this.state;
+
     e.preventDefault();
-    socket.emit("message", this.state.value)
-    this.setState({ value: '' })
+    socket.emit('message', { value });
+    this.setState({ value: '' });
   }
 
   render() {
+    const { value, messages } = this.state;
+
     return (
       <div className="App">
         <div className="App-intro">
           <p>socket.io test</p>
           <form onSubmit={this.handleSubmit}>
-            <input type="text" value={this.state.value} onChange={this.handleChange}></input>
-            <input type="submit"></input>
+            <input type="text" value={value} onChange={this.handleChange} />
+            <input type="submit" />
           </form>
           <ul>
-            {this.state.messages.map((message, i) => (<li key={i}>{message}</li>))}
+            {messages.map((message) => (<li key={message.id}>{`${message.user_id}: ${message.value}`}</li>))}
           </ul>
         </div>
       </div>
