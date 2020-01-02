782d75a0746bb0090c4eb35d9d359b5e02a295a9
Simon Tollstern <simontollstern@gmail.com>
2020-01-02 20:46:11 +0100

    Add timestamp to messages

--- server.js
+++ server.js
@@ -5,7 +5,12 @@ let messageId = 1;
 io.on('connection', socket => {
   console.log('a user connected');
   socket.on('message', message => {
-    const messageObject = { id: messageId, user: message.username, value: message.value };
+    const messageObject = {
+      id: messageId,
+      user: message.username,
+      value: message.value,
+      sentAt: new Date().toLocaleTimeString()
+    };
     console.log(messageObject);
     io.emit('message', messageObject);
     messageId += 1;
